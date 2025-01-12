5b9e5bc63fa9631b3a8551bb0a2fdadab920c60e
shubhjangir <shubhamjangir101@gmail.com>
2025-01-13 00:14:05 +0530

    Initial commit

---
+++ Components/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const Footer = () => {
+  const productList = ["Market", "ERC20 Token", "Donation"];
+  const contactList = [
+    "support@draitweb3.com",
+    "info@example.com",
+    "Contact us",
+  ];
+  const usefullLink = ["Home", "About Us", "Company Bio"];
+  return (
+    <footer class="text-center text-white backgroundMain lg:text-left">
+      <div class="mx-6 py-10 text-center md:text-left">
+        <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <div class="">
+            <h6 class="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+              CrowdFundingDrait
+            </h6>
+            <p>
+              Here you can contact us and all the information that is sufficient
+              are listed below. Thank You for visiting :)
+            </p>
+          </div>
+          <div class="">
+            <h6 class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Products
+            </h6>
+            {productList.map((el, i) => (
+              <p class="mb-4" key={i + 1}>
+                <a href="#!">{el}</a>
+              </p>
+            ))}
+          </div>
+          <div class="">
+            <h6 class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Useful links
+            </h6>
+            {usefullLink.map((el, i) => (
+              <p class="mb-4" key={i + 1}>
+                <a href="#!">{el}</a>
+              </p>
+            ))}
+          </div>
+          <div>
+            <h6 class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Contact
+            </h6>
+            {contactList.map((el, i) => (
+              <p class="mb-4" key={i + 1}>
+                <a href="#!">{el}</a>
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div class="backgroundMain p-6 text-center">
+        <span>© 2023 Copyright:</span>
+        <a class="font-semibold " href="https://tailwind-elements.com/">
+          BlockChain
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  // networks: {
+  //   polygon_mumbai: {
+  //     chainId: 80001,
+  //     url: "https://polygon-mumbai.g.alchemy.com/v2/0awa485pp03Dww2fTjrSCg7yHlZECw-K",
+  //     // url: process.env.POLYGON_MUMBAI,
+  //     accounts: [
+  //       `0x${"YOUR ADDRESS"}`,
+  //     ],
+  //   },
+  // },
+};
